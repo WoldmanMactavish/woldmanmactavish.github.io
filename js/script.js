cd1de103c8d5a6a32c74172f4e757d9457fb29c5
WoldmanMactavish <waldolautaro8947@outlook.es>
2021-02-12 19:13:52 -0300

    v1.0

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("icons").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+    document.getElementById("move-content").classList.toggle('move-container-all')
+    document.getElementById("show-menu-lateral").classList.toggle('show-lateral')
+}
